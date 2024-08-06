523a0634c2a71c0ecb3419b34a2c70587936e398
viamarkable <viamarkable@gmail.com>
2024-08-06 08:54:39 -0700

    starting caesar cipher code :D

---
+++ easy/archeological-dig/caesarCipher.java
@@ -0,0 +1,6 @@
+public class caesarCipher {
+    // Scanner reader = new Scanner(System.in);
+    // System.out.println("Would you like to encode a message? (y/n)");
+    // int n = reader.nextInt();
+    // reader.close();
+}
