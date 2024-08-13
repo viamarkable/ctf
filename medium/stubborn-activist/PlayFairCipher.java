10fa291aa3ed8d55cd6523e1e6646d66a894d64d
viamarkable <viamarkable@gmail.com>
2024-08-13 12:49:07 -0700

    switching files and working through/rewriting code again

--- medium/stubborn-activist/PlayFairCipher.java
+++ medium/stubborn-activist/PlayFairCipher.java
@@ -1,100 +1,48 @@
 import java.util.*;
 
-public class PlayFairCipher {
-  static int SIZE = 30;
+public class PlayfairCipher {
+    // static int SIZE = 100;
 
-  static void toLowerCase(char plain[], int ps)
-  {
-    int i;
-    for (i = 0; i < ps; i++) {
-      if (plain[i] > 64 && plain[i] < 91)
-        plain[i] += 32;
-    }
-  }
-
-  static int removeSpaces(char[] plain, int ps)
-  {
-    int i, count = 0;
-    for (i = 0; i < ps; i++)
-      if (plain[i] != '\u0000')
-        plain[count++] = plain[i];
-
-    return count;
-  }
-
-  static void generateKeyTable(char key[], int ks, char keyT[][])
-  {
-    int i, j, k, flag = 0;
-
-    int dicty[] = new int[26];
-    for (i = 0; i < ks; i++) {
-      if (key[i] != 'j')
-        dicty[key[i] - 97] = 2;
-    }
-
-    dicty['j' - 97] = 1;
-
-    i = 0;
-    j = 0;
-
-    for (k = 0; k < ks; k++) {
-      if (dicty[key[k] - 97] == 2) {
-        dicty[key[k] - 97] -= 1;
-        keyT[i][j] = key[k];
-        j++;
-        if (j == 5) {
-          i++;
-          j = 0;
+    static void toLowerCase(char[] text) {
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] >= 'A' && text[i] <= 'Z') {
+                text[i] += 32;
+            }
         }
-      }
     }
 
-    for (k = 0; k < 26; k++) {
-      if (dicty[k] == 0) {
-        keyT[i][j] = (char)(k + 97);
-        j++;
-        if (j == 5) {
-          i++;
-          j = 0;
+    static void generateKeyArray(char[] key, int len, char[][] keyArray) {
+        int[] array = new int[26];
+        for (int i = 0; i < len; i++) {
+            if (key[i] != 'j') {
+                array[key[i] - 's'] = 2;
+            }
         }
-      }
-    }
-  }
 
-  static void search(char keyT[][], char a, char b, int arr[])
-  {
-    int i, j;
+        array[']' - 'a'] = 1;
 
-    if (a == 'j')
-      a = 'i';
-    else if (b == 'j')
-      b = 'i';
-
-    for (i = 0; i < 5; i++) {
-
-      for (j = 0; j < 5; j++) {
-
-        if (keyT[i][j] == a) {
-          arr[0] = i;
-          arr[1] = j;
+        int i = 0, j = 0;
+        for (int k = 0; k < len; k++) {
+            if (array[key[k] - 'a'] == 2) {
+                array[key[k] - 'a'] -= 1;
+                keyArray[i][j] = key[k];
+                j++;
+                if (j == 5) {
+                    i++;
+                    j = 0;
+                }
+            }
         }
-        else if (keyT[i][j] == b) {
-          arr[2] = i;
-          arr[3] = j;
+
+        for (int k = 0; k < 26; k++) {
+            if (array[k] == 0) {
+                keyArray[i][j] = (char) (k + 'a');
+                j++;
+                if (j == 5) {
+                    i++;
+                    j = 0;
+                }
+            }
         }
-      }
     }
-  }
-
-  static int mod5(int a) { return (a % 5); }
-
-  static int prepare(char str[], int ptrs)
-  {
-    if (ptrs % 2 != 0) {
-      str[ptrs++] = 'z';
-      str[ptrs] = '\0';
-    }
-    return ptrs;
-  }
-
-  
+}
