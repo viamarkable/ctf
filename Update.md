49f5208a5f999d5483f238dfd332beff32b85fa9
viamarkable <viamarkable@gmail.com>
2024-08-29 12:24:17 -0700

    V2 prep and columnar h2

--- Update.md
+++ Update.md
@@ -2,5 +2,8 @@ Hello and welcome again to my CTF compilation! If you've gotten here, I assume t
 
 Here's just a quick place for me to place progress updates at the more major checkpoints, so that users have more information as to what is happening aside from the very basic, and likely neglected README.md >_<
 
+V2: Second release, August 29th 2024
+The amount of CTF puzzles have remained the same, but I've added many more solving scripts for each puzzle. There are scripts in both Java and Python, and I'm looking to expand to more C languages soon. However, some of the scripts have been denoted as not functional as there are small bugs that I still need to fix.
+
 V1: First release, August 7th 2024
 Currently, I have 11 CTF puzzles created (5 easy, 3 medium, 3 hard) and I've decided to put a pause to making puzzles so far. I have started writing the cipher code to solve them, using user input to plug into the algorithm. Only the caeserCipher.java file has been completed so far, and I'm working off that baseline for the vigenereCipher.java file, which is a big work in progress. There's not too much to tinker around with right now due to the lack of coding done so far, but there are puzzles with detailed writeups uploaded. Have fun playing! (25 hours)
--- hard/recipe-heist/ColumnarTranspositionCipher.java
+++ hard/recipe-heist/ColumnarTranspositionCipher.java
@@ -50,5 +50,52 @@ public class ColumnarTranspositionCipher {
         return cipher.toString();
     }
 
-    
+    public static String decryptMessage(String cipher) {
+        int col = key.length();
+
+        int row = (int) Math.ceil((double) cipher.length() / col);
+        char[][] cipherMat = new char[row][col];
+
+        int k = 0;
+        for (int j = 0; j < col; j++) {
+            for (int i = 0; i < row; i++) {
+                cipherMat[i][j] = cipher.charAt(k);
+                k++;
+            }
+        }
+
+        int index = 0;
+        for (Map.Entry<Character, Integer> entry : keyMap.entrySet()) {
+            entry.setValue(index++);
+        }
+
+        char[][] decCipher = new char[row][col];
+        for (int l = 0; l < key.length(); l++) {
+            int columnIndex = keyMap.get(key.charAt(l));
+            for (int i = 0; i < row; i++) {
+                decCipher[i][l] = cipherMat[i][columnIndex];
+            }
+        }
+
+        StringBuilder msg = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (decCipher[i][j] != '_') {
+                    msg.append(decCipher[i][j]);
+                }
+            }
+        }
+
+        return msg.toString();
+    }
+
+    public static void main(String[] args) {
+        
+        // tbd hehe
+
+        String cipher = encryptMessage(msg);
+        System.out.println("Encrypted Message: " + cipher);
+
+        System.out.println("Decrypted Message: " + decryptMessage(cipher));
+    }
 }
